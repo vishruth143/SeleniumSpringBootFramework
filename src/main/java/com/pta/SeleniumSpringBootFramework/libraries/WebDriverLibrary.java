587f5ca3389244ab0cfad0b8b2eb092f949c72a2
Vishvambruth J T <vishruth.cse@gmail.com>
2025-06-08 01:01:06 +0530

    Support cross-browser testing

--- src/main/java/com/pta/SeleniumSpringBootFramework/libraries/WebDriverLibrary.java
+++ src/main/java/com/pta/SeleniumSpringBootFramework/libraries/WebDriverLibrary.java
@@ -3,15 +3,39 @@ package com.pta.SeleniumSpringBootFramework.libraries;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 @Configuration
 public class WebDriverLibrary {
 	
+	@Value("${browser.type:chrome}") // Default browser is Chrome
+    private String browserType;
+	
 	@Bean
 	public WebDriver getChromeDriver() {
-		WebDriverManager.chromedriver().setup();
-		return new ChromeDriver();
+		WebDriver driver;
+		
+		switch (browserType.toLowerCase()) {
+        case "firefox":
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+            break;
+
+        case "edge":
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+            break;
+
+        case "chrome":
+        default:
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            break;
+    }
+		return driver;
 	}
 }
--- src/test/java/com/pta/SeleniumSpringBootFramework/SeleniumSpringBootFrameworkApplicationTests.java
+++ src/test/java/com/pta/SeleniumSpringBootFramework/SeleniumSpringBootFrameworkApplicationTests.java
@@ -3,6 +3,7 @@ package com.pta.SeleniumSpringBootFramework;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import com.pta.SeleniumSpringBootFramework.pages.MainPage;
 
 @SpringBootTest
